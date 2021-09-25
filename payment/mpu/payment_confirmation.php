22d0c95bbf2888ff89be946908ec8026fe87fb34
YeLinnAungGautam <davidgautam.1234@gmail.com>
2021-09-25 22:01:25 +0630

    Phin Yarrr

--- payment/mpu/payment_confirmation.php
+++ payment/mpu/payment_confirmation.php
@@ -39,8 +39,8 @@
     </header>
     <div style="margin-top: 100px;">
     <center>
-<!-- <form id="payment_confirmation" action="https://testsecureacceptance.cybersource.com/pay" method="post"/> -->
-    <form id="payment_confirmation" action="https://apitest.cybersource.com/pts/v2/payments" method="post"/>
+<form id="payment_confirmation" action="https://testsecureacceptance.cybersource.com/pay" method="post"/>
+    <!-- <form id="payment_confirmation" action="https://apitest.cybersource.com/pts/v2/payments" method="post"/> -->
     <table>
     <tbody>
     <tr>

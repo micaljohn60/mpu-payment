03e47dcf0ddc46e4463bdd30101ef0a1d018c6be
Thihan Hein(Michael Han) <39163166+micaljohn60@users.noreply.github.com>
2021-09-27 20:43:01 +0630

    Update

--- payment/mpu/db.php
+++ payment/mpu/db.php
@@ -1,9 +1,9 @@
 <?php
 //DB details
 $dbHost = 'localhost';
-$dbUsername = 'm9ecomme_m9';
-$dbPassword = '8;tHUBUk06V*';
-$dbName = 'm9ecomme_m9';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'm9ecommerence_m9';
 //Create connection and select DB
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 if($db->connect_error){

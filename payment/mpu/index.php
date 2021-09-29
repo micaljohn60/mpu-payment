41155bcabfca13efea548d2e051f3239693932b2
Thihan Hein(Michael Han) <39163166+micaljohn60@users.noreply.github.com>
2021-09-29 11:00:36 +0630

    Update

--- payment/mpu/index.php
+++ payment/mpu/index.php
@@ -813,7 +813,7 @@ function getHash($data)
 $hashValue = getHash($_form_data);
 echo $hashValue;
 
-?>" >
+?>" ">
 	
 	<a href="https://www.mpuecomuat.com:60145/UAT/Payment/Action/api?merchantID=<?php echo "201104000003541" ?>tranRef=<?php echo "9549" ?>actionType=<?php echo "I" ?>hashValue=<?php echo "34E8E91C29E73B9648011FADBAE19849B520B24A" ?>">Hello</a>
 	      <table>

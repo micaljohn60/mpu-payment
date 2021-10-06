397397190b8f4ceab5621f2e98eb051a9965d771
Thihan Hein(Michael Han) <39163166+micaljohn60@users.noreply.github.com>
2021-10-06 15:23:36 +0630

    Update

--- payment/mpu/merchantpost.php
+++ payment/mpu/merchantpost.php
@@ -8,7 +8,7 @@ echo $invalid_fields;
 echo $decision;
  include_once 'db.php';
        
-        $insert = $db->query("INSERT test_table () VALUES ('".$invalid_fields."')");
+        $insert = $db->query("INSERT test_table (invalid_field) VALUES ('".$invalid_fields."')");
 
       $insert2 = $db->query("UPDATE order_shippings SET status='$status' WHERE shipping_phone='$userDefined2' ");
  ?>

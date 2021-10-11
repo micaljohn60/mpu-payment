42601c3de9f1902a392ba504b66333a5aefe59a6
YeLinnAungGautam <87935026+YeLinnAungGautam@users.noreply.github.com>
2021-10-11 16:13:34 +0630

    Update visa_extra_info.php

--- payment/mpu/visa_extra_info.php
+++ payment/mpu/visa_extra_info.php
@@ -11,6 +11,17 @@ try{
     exit("Error: " .$e->getMessage());
 } 
 
-    echo "Hello"
+    $name = $_POST['name'];
+    $reference = $_POST['quantity'];
+    $quantity  = $_POST['reference_number'];
+    
+
+
+$sql = "INSERT INTO payment_visa(reference_number,username,quantity) VALUE (:reference_number,:username,:quantity)";
+$insert_query = $connection->prepare($sql);
+$insert_query->bindParam(':reference_number',$name,PDO::PARAM_STR);
+$insert_query->bindParam(':username',$reference,PDO::PARAM_STR);
+$insert_query->bindParam(':quantity',$quantity,PDO::PARAM_STR);
+$insert_query->execute();
 
 ?>

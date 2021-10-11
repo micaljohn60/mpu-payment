58bcad11d719749981b87dae72e53bae95e922b7
YeLinnAungGautam <87935026+YeLinnAungGautam@users.noreply.github.com>
2021-10-11 16:11:37 +0630

    Update visa_extra_info.php

--- payment/mpu/visa_extra_info.php
+++ payment/mpu/visa_extra_info.php
@@ -11,24 +11,6 @@ try{
     exit("Error: " .$e->getMessage());
 } 
 
-    $name = $_POST['bill_to_forename'];
-    $reference = $_POST['quantity'];
-    $quantity  = $_POST['reference_number'];
-    
-
-
-$sql = "INSERT INTO payment_visa(reference_number,username,quantity,transaction_id,req_card_number,card_type_name,reason_code,auth_amount,req_amount,decision) VALUE (:reference_number,:username,:quantity,:transaction_id,:req_card_number,:card_type_name,:reason_code,:auth_amount,:req_amount,:decision)";
-$insert_query = $connection->prepare($sql);
-$insert_query->bindParam(':reference_number',$name,PDO::PARAM_STR);
-$insert_query->bindParam(':username',$reference,PDO::PARAM_STR);
-$insert_query->bindParam(':quantity',$quantity,PDO::PARAM_STR);
-$insert_query->bindParam(':transaction_id',PDO::PARAM_NULL);
-$insert_query->bindParam(':req_card_number',PDO::PARAM_NULL);
-$insert_query->bindParam(':card_type_name',PDO::PARAM_NULL);
-$insert_query->bindParam(':reason_code',PDO::PARAM_NULL);
-$insert_query->bindParam(':auth_amount',PDO::PARAM_NULL);
-$insert_query->bindParam(':req_amount',PDO::PARAM_NULL);
-$insert_query->bindParam(':decision',PDO::PARAM_NULL);
-$insert_query->execute();
+    echo "Hello"
 
 ?>

54962b4ac5c14d2ed833c6a43ccb3a84328d55ee
YeLinnAungGautam <87935026+YeLinnAungGautam@users.noreply.github.com>
2021-10-12 11:42:47 +0630

    Update merchantpost.php

--- payment/mpu/merchantpost.php
+++ payment/mpu/merchantpost.php
@@ -23,29 +23,22 @@ $decision = $_REQUEST["decision"];
 // $name = "yelinnaung@dev";
 // $quantity = 20;
 
-// $sql_find_reference_number = "SELECT reference_number FROM payment_visa WHERE reference_number =:refernum";
-// $query_find_reference_number = $connection->prepare($sql_find_reference_number);
-// $query_find_reference_number->bindParam(':refernum',$_REQUEST["req_reference_number"],PDO::PARAM_STR);
-// $query_find_reference_number->execute();
-// $result = $query_find_reference_number->fetch(PDO::FETCH_ASSOC);
-// $ans = $result['reference_number'];
-$sql_insert_response = "INSERT INTO payment_visa(reference_number,transaction_id,req_card_number,card_type_name,reason_code,auth_amount,req_amount,decision) VALUE (:referencenub,:transaction_id,:req_card_number,:card_type_name,:reason_code,:auth_amount,:req_amount,:decision)";
-$insert_response_query = $connection->prepare($sql_insert_response);
-$insert_response_query->bindParam(':referencenub',$req_reference_number,PDO::PARAM_STR);
-// $insert_response_query->bindParam(':username',$name,PDO::PARAM_STR);
-// $insert_response_query->bindParam(':quantity',$quantity,PDO::PARAM_STR);
-$insert_response_query->bindParam(':transaction_id',$req_transaction_id,PDO::PARAM_STR);
-$insert_response_query->bindParam(':req_card_number',$req_card_number,PDO::PARAM_STR);
-$insert_response_query->bindParam(':card_type_name',$card_type_name,PDO::PARAM_STR);
-$insert_response_query->bindParam(':reason_code',$reason_code,PDO::PARAM_STR);
-$insert_response_query->bindParam(':auth_amount',$auth_amount,PDO::PARAM_STR);
-$insert_response_query->bindParam(':req_amount',$req_amount,PDO::PARAM_STR);
-$insert_response_query->bindParam(':decision',$decision,PDO::PARAM_STR);
-$insert_response_query->execute();
-//  if($ans == $_REQUEST["req_reference_number"]){
- $sql_update_response = "UPDATE payment_visa SET transaction_id = :transaction_id,req_card_number = :req_card_number,card_type_name = :card_type_name,reason_code = :reason_code,auth_amount = :auth_amount,req_amount = :req_amount,decision = :decision WHERE reference_number = :reqnumber";
+$sql_find_reference_number = "SELECT reference_number FROM payment_visa WHERE reference_number =:refernum";
+$query_find_reference_number = $connection->prepare($sql_find_reference_number);
+$query_find_reference_number->bindParam(':refernum',$_REQUEST["req_reference_number"],PDO::PARAM_STR);
+$query_find_reference_number->execute();
+$result = $query_find_reference_number->fetchAll(PDO::FETCH_OBJ);
+foreach($result as $row){
+  $ref_number = $row->reference_number;
+  $username = $row->username;
+  $quantity = $row->quantity;
+ 
+ if($ref_number == $_REQUEST["req_reference_number"]){
+$sql_update_response = "UPDATE payment_visa SET username = :username, quantity = :quantity, transaction_id = :transaction_id,req_card_number = :req_card_number,card_type_name = :card_type_name,reason_code = :reason_code,auth_amount = :auth_amount,req_amount = :req_amount,decision = :decision WHERE reference_number = :reqnumber";
 $query_update_response = $connection->prepare($sql_update_response);
 $query_update_response->bindParam(':reqnumber',$req_reference_number,PDO::PARAM_STR);
+$query_update_response->bindParam(':username',$username,PDO::PARAM_STR);
+  $query_update_response->bindParam(':quantity',$quantity,PDO::PARAM_STR);
 $query_update_response->bindParam(':transaction_id',$req_transaction_id,PDO::PARAM_STR);
 $query_update_response->bindParam(':req_card_number',$req_card_number,PDO::PARAM_STR);
 $query_update_response->bindParam(':card_type_name',$card_type_name,PDO::PARAM_STR);
@@ -54,9 +47,10 @@ $query_update_response->bindParam(':auth_amount',$auth_amount,PDO::PARAM_INT);
 $query_update_response->bindParam(':req_amount',$req_amount,PDO::PARAM_INT);
 $query_update_response->bindParam(':decision',$decision,PDO::PARAM_STR);
 $query_update_response->execute();
-//  }else{
-//    echo "Not Updated";
-// }
+ }else{
+   echo "Not Updated";
+}
+}
 ?>
 
 

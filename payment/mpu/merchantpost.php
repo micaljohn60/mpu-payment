1f65ad8e7a505319d77da7e94862763b2078e9a5
YeLinnAungGautam <87935026+YeLinnAungGautam@users.noreply.github.com>
2021-10-12 10:29:01 +0630

    Update merchantpost.php

--- payment/mpu/merchantpost.php
+++ payment/mpu/merchantpost.php
@@ -10,8 +10,8 @@ try{
 }catch(PDOException $e){
     exit("Error: " .$e->getMessage());
 } 
-
-$req_reference_number = $_REQUEST["req_reference_number"];
+$req_reference_number = VemoNerI20920ts;
+// $req_reference_number = $_REQUEST["req_reference_number"];
 $req_transaction_id = $_REQUEST["transaction_id"];
 $req_card_number = $_REQUEST["req_card_number"];
 $card_type_name = $_REQUEST["card_type_name"];
@@ -21,6 +21,16 @@ $req_amount = $_REQUEST["req_amount"];
 $decision = $_REQUEST["decision"];
 $name = "yelinnaung@dev";
 $quantity = 20;
+
+$sql_find_reference_number = "SELECT reference_number FROM payment_visa WHERE reference_number =:refernum";
+$query_find_reference_number = $connection->prepare($sql_find_reference_number);
+$query_find_reference_number->bindParam(':refernum',$req_reference_number,PDO::PARAM_STR);
+$query_find_reference_number->execute();
+$result = $query_find_reference_number->fetch(PDO::FETCH_ASSOC);
+$ans = $result['reference_number'];
+
+echo $ans;
+
 
 $sql_update_response = "UPDATE payment_visa SET(transaction_id = :transaction_id,req_card_number = :req_card_number,card_type_name = :card_type_name,reason_code = :reason_code,auth_amount = :auth_amount,req_amount = :req_amount,decision = :decision) WHERE reference_number = :reqnumber";
 $query_update_response = $connection->prepare($sql_update_response);

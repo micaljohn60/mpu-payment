51b1ea767e46431db46ece64c14afc6b619eef82
YeLinnAungGautam <87935026+YeLinnAungGautam@users.noreply.github.com>
2021-10-12 10:46:04 +0630

    Update payment_confirmation.php

--- payment/mpu/payment_confirmation.php
+++ payment/mpu/payment_confirmation.php
@@ -17,12 +17,15 @@ try{
     $name = $_POST['bill_to_forename'];
     $reference = $_POST['reference_number'];
     $quantity  = $_POST['quantity'];
+    $transaction_id = 0;
 
-$sql = "INSERT INTO payment_visa(reference_number,username,quantity) VALUE (:reference_number,:username,:quantity)";
+$sql = "INSERT INTO payment_visa(reference_number,username,quantity,transaction_id) VALUE (:reference_number,:username,:quantity,:transaction_id)";
 $insert_query = $connection->prepare($sql);
 $insert_query->bindParam(':reference_number',$reference,PDO::PARAM_STR);
 $insert_query->bindParam(':username',$name,PDO::PARAM_STR);
 $insert_query->bindParam(':quantity',$quantity,PDO::PARAM_STR);
+$insert_query->bindParam(':transaction_id',$transaction_id,PDO::PARAM_STR);
+
 $insert_query->execute();
 
 ?>

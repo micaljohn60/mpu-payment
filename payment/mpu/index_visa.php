4d7009c026ac8c2b9e437c76c4b652581cf9fe38
YeLinnAungGautam <87935026+YeLinnAungGautam@users.noreply.github.com>
2021-10-26 15:50:36 +0630

    Update index_visa.php

--- payment/mpu/index_visa.php
+++ payment/mpu/index_visa.php
@@ -1,5 +1,20 @@
 <?php
 
+<?php
+ include 'security.php';
+ define('DB_HOST','localhost');
+ define('DB_USER','neptrior_mnine');
+ define('DB_PASS','Pas$m9db');
+ define('DB_NAME','neptrior_mninedb');
+ 
+try{
+    $connection = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PASS);
+    echo "Connection Success";
+}catch(PDOException $e){
+    echo $e->getMessage();
+    exit("Error: " .$e->getMessage());
+} 
+
 $email = $_POST['order_email'];
 $name = $_POST['customer_name'];
 $phone = $_POST['order_phone'];

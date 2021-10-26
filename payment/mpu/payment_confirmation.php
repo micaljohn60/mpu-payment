41316b473453891a4896da8fa3197537f22af542
YeLinnAungGautam <87935026+YeLinnAungGautam@users.noreply.github.com>
2021-10-26 15:46:34 +0630

    Update payment_confirmation.php

--- payment/mpu/payment_confirmation.php
+++ payment/mpu/payment_confirmation.php
@@ -17,19 +17,19 @@ try{
 
 
 
-$name = $_POST['bill_to_forename'];
-$name1 = $_POST['bill_to_forename'];
-$quantity  = $_POST['quantity'];
-$item_name = $_POST["item_#_name"];
-$reference_number = $_POST["reference_number"];
+// $name = $_POST['bill_to_forename'];
+// $name1 = $_POST['bill_to_forename'];
+// $quantity  = $_POST['quantity'];
+// $item_name = $_POST["item_#_name"];
+// $reference_number = $_POST["reference_number"];
 	
-$sql = "INSERT INTO payment_visa(reference_number,username,quantity,item_name) VALUE (:refnumber,:username,:quantity,:itemname)";
-$insert_query = $connection->prepare($sql);
-$insert_query->bindParam(':refnumber',$refrence_number,PDO::PARAM_STR);
-$insert_query->bindParam(':username',$name,PDO::PARAM_STR);
-$insert_query->bindParam(':quantity',$quantity,PDO::PARAM_STR);
-$insert_query->bindParam(':itemname',$item_name,PDO::PARAM_STR);
-$insert_query->execute();
+// $sql = "INSERT INTO payment_visa(reference_number,username,quantity,item_name) VALUE (:refnumber,:username,:quantity,:itemname)";
+// $insert_query = $connection->prepare($sql);
+// $insert_query->bindParam(':refnumber',$refrence_number,PDO::PARAM_STR);
+// $insert_query->bindParam(':username',$name,PDO::PARAM_STR);
+// $insert_query->bindParam(':quantity',$quantity,PDO::PARAM_STR);
+// $insert_query->bindParam(':itemname',$item_name,PDO::PARAM_STR);
+// $insert_query->execute();
 
 
 

068b6caf83f24c346a3a60f0e4c5aa03635917df
Thihan Hein(Michael Han) <39163166+micaljohn60@users.noreply.github.com>
2021-11-05 21:24:11 +0630

    Update

--- payment/mpu/index_visa.php
+++ payment/mpu/index_visa.php
@@ -903,8 +903,8 @@ if (empty($email)) {
     <input type="hidden" name="access_key" value="9d5568c01f83317bb1dc7ae74a3484ac">
     <input type="hidden" name="profile_id" value="FD26AD45-FADF-4239-9D62-9E99D4B4F412">
     <input type="hidden" name="transaction_uuid" value="<?php echo uniqid() ?>">
-    <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,line_item_count">
-<input type="hidden" name="unsigned_field_names" value="line_item_count">
+    <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,">
+<input type="hidden" name="unsigned_field_names" value="line_item_count,item_0_unit_price,item_1_unit_price,item_2_unit_price,item_1_name,item_2_code,item_3_quantity">
 
     
 

605599860023edb3e859ce1d3186fd101d4c220a
Thihan Hein(Michael Han) <39163166+micaljohn60@users.noreply.github.com>
2021-11-09 20:28:16 +0630

    Update

--- payment/mpu/merchantpost.php
+++ payment/mpu/merchantpost.php
@@ -10,7 +10,12 @@ try{
 }catch(PDOException $e){
     exit("Error: " .$e->getMessage());
 } 
-$req_name = $_REQUEST["req_item_1_passenger_forename"];
+
+$decision = $_REQUEST["decision"];
+
+
+if($decision == "ACCEPT" || $decision == "DECLINE"){
+    $req_name = $_REQUEST["req_item_1_passenger_forename"];
 $req_item_name = $_REQUEST["req_item_1_name"];
 $req_item_specification = $_REQUEST["req_item_1_code"];
 $req_item_quantity = $_REQUEST["req_item_1_quantity"];
@@ -21,10 +26,6 @@ $card_type_name = $_REQUEST["card_type_name"];
 // $reason_code = $_REQUEST["reason_code"];
 $auth_amount = $_REQUEST["auth_amount"];
 $req_amount = $_REQUEST["req_amount"];
-$decision = $_REQUEST["decision"];
-
-
-if($decision == "ACCEPT" || $decision == "DECLINE"){
 $sql_insert_response = "INSERT INTO payment_visa_confirmation(req_bill_to_forename,req_item_name,req_item_color,req_item_quantity,req_reference_number,transaction_id,req_card_number,card_type_name,auth_amount,req_amount,decision) VALUE (:reqname,:reqitemname,:reqitemspecification,:reqitemquantity,:referencenub,:transaction_id,:req_card_number,:card_type_name,:auth_amount,:req_amount,:decision)" ;
 $insert_response_query = $connection->prepare($sql_insert_response);
 $insert_response_query->bindParam(':reqname',$req_name,PDO::PARAM_STR);
@@ -40,6 +41,18 @@ $insert_response_query->bindParam(':req_amount',$req_amount,PDO::PARAM_STR);
 $insert_response_query->bindParam(':decision',$decision,PDO::PARAM_STR);
 $insert_response_query->execute();
 }if($decision == "CANCEL"){
+    $req_name = $_REQUEST["req_item_1_passenger_forename"];
+$req_item_name = $_REQUEST["req_item_1_name"];
+$req_item_specification = $_REQUEST["req_item_1_code"];
+$req_item_quantity = $_REQUEST["req_item_1_quantity"];
+$req_reference_number = $_REQUEST["req_reference_number"];
+$req_transaction_id = $_REQUEST["transaction_id"];
+$req_card_number = $_REQUEST["req_card_number"];
+$card_type_name = $_REQUEST["card_type_name"];
+// $reason_code = $_REQUEST["reason_code"];
+$auth_amount = $_REQUEST["auth_amount"];
+$req_amount = $_REQUEST["req_amount"];
+    
 $sql_insert_response = "INSERT INTO payment_visa_confirmation(req_bill_to_forename,req_item_name,req_item_color,req_item_quantity,req_reference_number,transaction_id,req_card_number,card_type_name,auth_amount,req_amount,decision) VALUE (:reqname,:reqitemname,:reqitemspecification,:reqitemquantity,:referencenub,:transaction_id,:req_card_number,:card_type_name,:auth_amount,:req_amount,:decision)" ;
 $insert_response_query = $connection->prepare($sql_insert_response);
 $insert_response_query->bindParam(':reqname',$req_name,PDO::PARAM_STR);
